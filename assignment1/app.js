3367d4ae75333f2905f549545bd21abb6b832236
ajmalmuhammed <muhammedajmal54@gmail.com>
2021-02-03 13:11:45 +0530

    Minor fix

--- assignment1/app.js
+++ assignment1/app.js
@@ -15,9 +15,14 @@ function LunchCheckController ($scope) {
   $scope.display = function () {
     console.log("values:")
     console.log($scope.values);
-    var noOfItems = calNos($scope.values);
+    var textOutput;
+    if ($scope.values == "" )
+       textOutput = " Please enter data first";
 
-    $scope.output = noOfItems;
+    else 
+      textOutput = calNos($scope.values);
+
+    $scope.output = textOutput;
     
 
   };
@@ -26,11 +31,11 @@ function LunchCheckController ($scope) {
     var len = (string.split(',').length);
     if(  len >3)
       newOutput = "Too much!";
-    else if(len <= 3 && len>1 )
+    else if(len <= 3 && len>=1 )
       newOutput = "Enjoy!"
-    else if (len <=1)
-      newOutput = "Please enter data first"
-    
+    // else if (len <=1 )
+    //   newOutput = "Please enter data first"
+    console.log(len);
     console.log(newOutput);
     return newOutput;
    
